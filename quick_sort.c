3568abc41938068e27ab8c72b25a120ae9f3dcb0
Mayank Banerjee <mayankbanerjee04@gmail.com>
2024-04-24 20:57:56 +0530

    New Commit

--- quick_sort.c
+++ quick_sort.c
@@ -11,33 +11,6 @@ void swap(int *a, int *b){
 }
 
 int partition(int low, int high) {
-  /*
-  // select the rightmost element as pivot
-  int pivot = a[high];
-  
-  // pointer for greater element
-  int i = (low - 1);
-
-  // traverse each element of the array
-  // compare them with the pivot
-  for (int j = low; j < high; j++) {
-    if (a[j] <= pivot) {
-        
-      // if element smaller than pivot is found
-      // swap it with the greater element pointed by i
-      i++;
-      
-      // swap element at i with element at j
-      swap(&a[i], &a[j]);
-    }
-  }
-
-  // swap the pivot element with the greater element at i
-  swap(&a[i + 1], &a[high]);
-  
-  // return the partition point
-  return (i + 1);*/
-
   int i,j,t,flag=1,pos=low;
   while(flag == 1){
     for(i=high;i>pos;i--){
