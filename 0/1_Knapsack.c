d6a55596107c391388d66fabdd264e0ce687423b
Mayank Banerjee <mayankbanerjee04@gmail.com>
2024-05-25 09:49:35 +0530

    New Commit

---
+++ 0/1_Knapsack.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+// Returns the maximum value that
+// can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int K[n + 1][W + 1];
+
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1]
+                                  + K[i - 1][w - wt[i - 1]],
+                              K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    return K[n][W];
+}
+
+// Driver Code
+int main()
+{
+    /*int profit[] = { 60, 100, 120 };
+    int weight[] = { 10, 20, 30 };
+    int W = 50;*/
+    int i, n;
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    int profit[n], weight[n], W;
+    printf("Enter the profits and weights:\n");
+    for(i=0;i<n;i++){
+        printf("Item %d\n", i+1);
+        printf("Profit: ");
+        scanf("%d", &profit[i]);
+        printf("Weight: ");
+        scanf("%d", &weight[i]);
+        printf("\n");
+    }
+
+    printf("Enter the capacity of the Knapsack: ");
+    scanf("%d", &W);
+    
+    printf("Profit: %d", knapSack(W, weight, profit, n));
+    return 0;
+}
